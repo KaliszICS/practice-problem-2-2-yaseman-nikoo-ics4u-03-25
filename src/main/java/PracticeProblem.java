bd085672cb1d7569ccbb346a3b8f900a2613c1dc
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-20 21:45:51 +0000

    complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,7 +12,7 @@ public class PracticeProblem {
 				i = array.length;
 			}
 		}
-		int binary=-1;
+		int binary=0;
 		int high = array.length-1;
 		int low =0;
 		int mid;
@@ -20,11 +20,11 @@ public class PracticeProblem {
 		while (low<=high){
 			
 			mid =(high+low)/2;
-			if (mid == number){
+			if (array[mid] == number){
 				low = high+1;
 				binary++;
 			}
-			else if(number> mid){
+			else if(number> array[mid]){
 				low = mid+1;
 				binary++;
 			}
@@ -37,5 +37,34 @@ public class PracticeProblem {
 		int[] intArray = {sequential, binary};
 		return intArray;
 	}
-	
+	public static int[] compareStringSearch(String[] array, String word){
+		int sequential=0;
+		for(int i=0; i<array.length; i++){
+			sequential++;
+			if (array[i].equals(word)){
+				i=array.length;
+			}
+		}
+		int binary=0;
+		Arrays.sort(array);
+		int high=array.length;
+		int low=0;
+		int mid;
+		while(high>=low){
+			binary++;
+			mid= (high+low)/2;
+			if (array[mid].equals(word)){
+				low=high+1;
+			}
+			else if(array[mid].compareTo(word)<0){
+				low=mid+1;
+			}
+			else{
+				high=mid-1;
+			}
+		
+		}	
+		int[] answer={sequential, binary};
+		return answer;
+	}
 }
